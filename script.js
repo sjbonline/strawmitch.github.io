3a49a675cfec403b3402c8a0353f77e1c81d7fca
Stephanie Brown <strawmitch@gmail.com>
2015-02-13 04:21:49 -0500

    first attempt
    user page

---
+++ script.js
@@ -0,0 +1,7 @@
+var moreBio = document.getElementById("more-bio");
+ 
+function expandBio() {
+  var hiddenText = document.getElementById("more-bio-text");
+  hiddenText.style.display ="inline";
+}
+moreBio.addEventListener("click", expandBio, false);
