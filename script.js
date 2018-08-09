83aa3edd2d6ef322b52cb5f7d7b404e0fe3d8a67
Stephanie Brown <strawmitch@gmail.com>
2018-08-09 09:54:02 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var moreBio = document.getElementById("more-bio");
- 
-function expandBio() {
-  var hiddenText = document.getElementById("more-bio-text");
-  hiddenText.style.display ="inline";
-}
-moreBio.addEventListener("click", expandBio, false);
-
-expandBio();
